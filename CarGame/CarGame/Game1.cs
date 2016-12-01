833d8371b25e365f29efa91377b1fad47c28eb3b
wsands1701 <will.sands.v@gmail.com>
2016-12-01 10:45:41 -0500

    Removed-Test-Comments

--- CarGame/CarGame/Game1.cs
+++ CarGame/CarGame/Game1.cs
@@ -1,7 +1,7 @@
 ﻿using Microsoft.Xna.Framework;
 using Microsoft.Xna.Framework.Graphics;
 using Microsoft.Xna.Framework.Input;
-//austin's code
+
 namespace CarGame
 {
     //I'm watching you all ~ McCloskey
