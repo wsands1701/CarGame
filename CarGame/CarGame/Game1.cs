56df7d85ad69ebfdbf05773ef8b9cfa2ba916a79
wsands1701 <will.sands.v@gmail.com>
2016-12-05 13:05:27 -0500

    added a comment

--- CarGame/CarGame/Game1.cs
+++ CarGame/CarGame/Game1.cs
@@ -5,7 +5,7 @@ using Microsoft.Xna.Framework.Input;
 namespace CarGame
 {
     //I'm watching you all ~ McCloskey
-   
+   //build it
     
     /// <summary>
     /// This is the main type for your game.
