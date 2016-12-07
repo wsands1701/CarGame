9dd2b5233aa5456efbc27215b844cc2dc8ae74c6
tyung82 <tyung@sewickley.org>
2016-12-07 14:36:11 -0500

    created getmouseposition method

--- CarGame/CarGame/Player.cs
+++ CarGame/CarGame/Player.cs
@@ -1,4 +1,7 @@
-﻿using System;
+﻿using Microsoft.Xna.Framework;
+using Microsoft.Xna.Framework.Graphics;
+using Microsoft.Xna.Framework.Input;
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
@@ -8,5 +11,18 @@ namespace CarGame
 {
     class Player
     {
+        Texture2D player;
+
+        public Point GetMousePosition()
+        {
+            return new Point(Mouse.GetState().X, Mouse.GetState().Y); 
+        }
+
+        
+
+        
     }
+
+    
+
 }
