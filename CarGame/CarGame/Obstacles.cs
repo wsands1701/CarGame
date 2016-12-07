6826ec65f5841d5ffb87a9e38a00d23346df274e
theletterg <bweir27@gmail.com>
2016-12-07 14:30:46 -0500

    made a toDo list

--- CarGame/CarGame/Obstacles.cs
+++ CarGame/CarGame/Obstacles.cs
@@ -8,5 +8,23 @@ namespace CarGame
 {
     class Obstacles
     {
+        new object car1(int xCoord, int yCoord, Boolean collison, int speed);
+
+
+        
     }
+
+
+
+    /* What we need to do:
+    make an ArrayList of Obejcts that we can add/subtract from. Each object will need to have the following properties:
+                -Spawning position
+                -speed (tree vs. oncoming traffic)
+                -collision (boolean)
+
+    Make a Traffic Class
+        -adds color
+
+
+    */
 }
