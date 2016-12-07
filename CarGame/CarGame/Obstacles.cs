23fcb9f9d771069dec3348878ee24ee5a3e8bae9
theletterg <bweir27@gmail.com>
2016-12-07 14:30:46 -0500

    made a toDo list

--- CarGame/CarGame/Obstacles.cs
+++ CarGame/CarGame/Obstacles.cs
@@ -6,75 +6,20 @@ using System.Threading.Tasks;
 
 namespace CarGame
 {
-    abstract class Obstacles
+    class Obstacles
     {
-        private int x;
-        private int y;
-        private int wastfilly;
-        Boolean Collide;
+        new object car1(int xCoord, int yCoord, Boolean collison, int speed);
 
-        //overloaded constructor method
-        public Obstacles(int x,int y, int speed, )
-            {
-
-            } 
-
-        //Gets
         public int getX()
         {
             return x;
         }
-
+        
         public int getY()
-    {
+        {
             return y;
-        }
-        private int getWastFilly()
-        {
-            return wastfilly;
-        }
-        public Boolean getCollide()
-        {
-            return Collide;
-        }
-
-        //Sets
-        public void setX(int X)
-        {
-            x = X;
-        }
-        public void setY(int Y)
-        {
-            y = Y;
-        }
-        
-        public void setWastFilly(int WastFilly)
-        {
-            wastfilly = WastFilly;
-           
-        }
-        public void setCollide(Boolean collide)
-        {
-            Collide = collide;
-        }
-
-        new object car1(int xCoord, int yCoord, Boolean collison, int speed);
-
-
-        
     }
 
 
-
-    /* What we need to do:
-    make an ArrayList of Obejcts that we can add/subtract from. Each object will need to have the following properties:
-                -Spawning position
-                -speed (tree vs. oncoming traffic)
-                -collision (boolean)
-
-    Make a Traffic Class
-        -adds color
-
-
-    */
+    }
 }
