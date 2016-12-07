555c07c2ddf63884a96edaad6f09c2ac574b435d
Arnastofesto <arnastofesto@gmail.com>
2016-12-07 14:35:22 -0500

    DANK

--- CarGame/CarGame/Obstacles.cs
+++ CarGame/CarGame/Obstacles.cs
@@ -17,10 +17,28 @@ namespace CarGame
         }
 
         public int getY()
-        {
+    {
             return y;
         }
 
 
+        new object car1(int xCoord, int yCoord, Boolean collison, int speed);
+
+
+        
     }
+
+
+
+    /* What we need to do:
+    make an ArrayList of Obejcts that we can add/subtract from. Each object will need to have the following properties:
+                -Spawning position
+                -speed (tree vs. oncoming traffic)
+                -collision (boolean)
+
+    Make a Traffic Class
+        -adds color
+
+
+    */
 }
