5bc291aff8026840516e7f7ee78f241700ca0d8f
peter-goebel <pgoebel@sewickley.org>
2017-01-11 12:48:12 -0500

    merge

--- CarGame/CarGame/Game1.cs
+++ CarGame/CarGame/Game1.cs
@@ -550,7 +550,7 @@ namespace CarGame
             spriteBatch.Draw(tree, treeRectangle5, Color.White);
             spriteBatch.Draw(whiteCar, playerRectangle, plCl);
             
-            spriteBatch.DrawString(font, "Points: " +  t1.TotalSeconds.ToString("####.##"), new Vector2(1050, 25),Color.White);
+            spriteBatch.DrawString(font, "Points: " +  t1.TotalSeconds.ToString("####.##"), new Vector2(1000, 25),Color.White);
         }
         public void DisplayHelpScreen()
         {
