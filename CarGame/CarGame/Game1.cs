d08bca6ea955a8e5936058f8ae1388ffd4c854ad
abkicks <abkicks1@gmail.com>
2017-01-13 14:19:07 -0500

    fixed help screen text

--- CarGame/CarGame/Game1.cs
+++ CarGame/CarGame/Game1.cs
@@ -647,7 +647,7 @@ namespace CarGame
         public void DisplayHelpScreen()
         {
             GraphicsDevice.Clear(Color.Black);
-            spriteBatch.DrawString(font, "\n-To move your car, click and hold the left mouse button and drag the \n wherever you want it to go.\n-Avoid obsticles traveling towards your car for the longest time to win!\n-Click the 'Play' button in to start game.\n-To quit, click the 'End' button. \n-To change the color of the car, click the 'Choose Color' button \n to select one of the colors.", new Vector2(50, 100), Color.White);
+            spriteBatch.DrawString(font, "\n-To move your car, click and hold the left mouse button and drag the mouse wherever you want the car to go.\n-Avoid obsticles traveling towards your car for the longest time to win!\n-Click the 'Play' button in to start game.\n-To quit, click the 'End' button. \n-To change the color of the car, click the 'Choose Color' button.", new Vector2(50, 100), Color.White);
             spriteBatch.Draw(back, backRectangle, Color.White);
             
         }
